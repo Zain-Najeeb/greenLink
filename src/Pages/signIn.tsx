0867c1301b3d69d4e91de420d0c98c05007a9ea4
thanarul <thanarul04@gmail.com>
2024-11-06 21:50:04 -0500

    thanush's changes

---
+++ src/Pages/signIn.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text } from "react-native";
+
+const SignIn = () => {
+	return (
+		<Text style = {styles.text}>Hello World!</Text>
+	);
+}
+const styles = StyleSheet.create({
+	text:{
+		color: "#00FF00"
+	}
+})
+export default SignIn;
