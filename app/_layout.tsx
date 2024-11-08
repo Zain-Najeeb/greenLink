8d5ff4f308321f2edbacf12e97c848e88b8b6b31
--replace-all <--replace-all>
2024-11-08 14:03:27 -0500

    safeAreView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,23 @@
 import { Stack } from "expo-router";
 import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
 import { primaryColour } from "@/constants/Colors";
 
 const RootLayout = () => {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(auth)/users" />
-    </Stack>
+    <SafeAreaView style={styles.safeArea}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)/users" />
+      </Stack>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: primaryColour,
+  },
+});
 
 export default RootLayout;
