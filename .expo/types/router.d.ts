e1d7588a820104d06f1b45fc39a8f45ccb9d2c9b
ProgrammedByHussain <hussainalshammari04@gmail.com>
2024-11-07 22:56:16 -0500

    testing

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/_sitemap` | `/users/forgotPassword` | `/users/signIn` | `/users/signUp`;
+      StaticRoutes: `/` | `/_sitemap` | `/users/forgotPassword` | `/users/signIn` | `/users/signUp`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
---
+++ app/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import SignInScreen from "./users/signIn"; // Adjust path as necessary
+
+export default function Index() {
+  return <SignInScreen />;
+}
