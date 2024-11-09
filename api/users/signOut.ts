f06e10ef1ee6632a550128e28776a33f7329e7fb
Navid Amin <mynameisnavid@gmail.com>
2024-11-09 16:05:39 -0500

    removed id param for inserting to table

--- api/users/signOut.ts
+++ api/users/signOut.ts
@@ -2,11 +2,11 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { supabase } from '../../util/supabaseClient'
 
 export const signOut = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-        console.error("Error signing out:", error.message);
-      } else {
-        await AsyncStorage.removeItem("supabase_session");
-        console.log("User signed out successfully");
-      }
+  const { error } = await supabase.auth.signOut();
+  if (error) {
+    console.error("Error signing out:", error.message);
+  } else {
+    await AsyncStorage.removeItem("supabase_session");
+    console.log("User signed out successfully");
+  }
 }
